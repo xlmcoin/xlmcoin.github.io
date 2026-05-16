3378aad5a4e60ca1b38a420c98aef026b6bd7deb
XLMCOIN <parsunnetwork@gmail.com>
2026-05-16 16:27:58 +0000

    Delete basic.js

--- basic.js
+++
@@ -1,8 +0,0 @@
-// Import the Stellar SDK
-const StellarSdk = require('stellar-sdk');
-
-// Define the asset with the name "SEC" and the given issuer address
-const SEC = new StellarSdk.Asset("SEC", "GDLLGBPQSKLJ2WX2MKEJ4PK24MR3D4GJLKGUFIWCJHU7EGTJ27EUDSEC");
-
-// Log the asset to verify
-console.log(SEC);
